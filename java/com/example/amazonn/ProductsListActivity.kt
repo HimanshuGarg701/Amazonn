1ee2bd334956e3aa216ea1b1596e86fd448e937a
Himanshu Garg <hgarg701@gmail.com>
2020-03-21 15:11:46 -0700

    Added shopping cart to collect products

--- java/com/example/amazonn/ProductsListActivity.kt
+++ java/com/example/amazonn/ProductsListActivity.kt
@@ -1,12 +1,29 @@
 package com.example.amazonn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class ProductsListActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recycler_view_products_list)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.products_cart, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.cart ->{
+                val intent = Intent(this, ShoppingCartActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return onOptionsItemSelected(item)
     }
 }
