40423377a0b6c497392886e221b82cdf3434538f
Himanshu Garg <hgarg701@gmail.com>
2020-03-20 23:47:28 -0700

    Added layout for products list

--- java/com/example/amazonn/ProductsListActivity.kt
+++ java/com/example/amazonn/ProductsListActivity.kt
@@ -3,10 +3,10 @@ package com.example.amazonn
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ProductsListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.recycler_view_products_list)
     }
 }
