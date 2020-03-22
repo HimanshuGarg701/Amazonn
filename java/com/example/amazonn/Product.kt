d5fc6deffdb8e837a3819443ded888c9f8d8f02a
Himanshu Garg <hgarg701@gmail.com>
2020-03-22 16:48:24 -0700

    Added room database for local cache of products

--- java/com/example/amazonn/Product.kt
+++ java/com/example/amazonn/Product.kt
@@ -1,4 +1,21 @@
 package com.example.amazonn
 
-data class Product(val name: String, val quantity: Int, val description : String) {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "product_data")
+data class Product(
+
+    @PrimaryKey(autoGenerate = true)
+    val productId : Long,
+
+    @ColumnInfo(name = "product_name")
+    val name: String,
+
+    @ColumnInfo(name = "product_quantity")
+    val quantity: Int,
+
+    @ColumnInfo(name="product_description")
+    val description : String) {
 }
